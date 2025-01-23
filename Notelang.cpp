145ecbbee3c753fcd37b030e0952aa76f26ad62a
root <root@JOHNRYCCABE4F59.>
2025-01-23 14:09:34 -0500

    pre configured files for project

---
+++ Notelang.cpp
@@ -0,0 +1,12 @@
+﻿// Notelang.cpp : Defines the entry point for the application.
+//
+
+#include "Notelang.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
